b804661d142d718469c50a93764098fa0955c4a0
suhang <13314901+suhangas@user.noreply.gitee.com>
2024-03-13 19:45:49 +0800

    consul

--- common/consul.go
+++ common/consul.go
@@ -21,43 +21,60 @@ func ConsulClient(addr string) (error, *api.Client) {
 	}
 	return nil, ConsulCli
 }
-func GetIp() (string, error) {
-	interfaces, err := net.Interfaces()
+
+//	func GetIp() (string, error) {
+//		interfaces, err := net.Interfaces()
+//		if err != nil {
+//			return "", err
+//		}
+//
+//		for _, i := range interfaces {
+//			addrs, err := i.Addrs()
+//			if err != nil {
+//				continue
+//			}
+//
+//			for _, addr := range addrs {
+//				ipNet, isVailIpNet := addr.(*net.IPNet)
+//				if isVailIpNet && !ipNet.IP.IsLoopback() {
+//					if ipNet.IP.To4() != nil {
+//						// 添加一些额外的检测逻辑，例如判断IP地址是否在本地网络范围内
+//						if ipNet.IP.IsGlobalUnicast() {
+//							// 添加详细的日志输出
+//							log.Printf("获取到的IP地址：%s，对应网络接口：%s\n", ipNet.IP.String(), i.Name)
+//							return ipNet.IP.String(), nil
+//						}
+//					}
+//				}
+//			}
+//		}
+//
+//		return "", errors.New("Unable to find a valid global unicast IP address")
+//	}
+func GetIp() (ip []string) {
+	addrs, err := net.InterfaceAddrs()
 	if err != nil {
-		return "", err
+		return ip
 	}
-
-	for _, i := range interfaces {
-		addrs, err := i.Addrs()
-		if err != nil {
-			continue
-		}
-
-		for _, addr := range addrs {
-			ipNet, isVailIpNet := addr.(*net.IPNet)
-			if isVailIpNet && !ipNet.IP.IsLoopback() {
-				if ipNet.IP.To4() != nil {
-					// 添加一些额外的检测逻辑，例如判断IP地址是否在本地网络范围内
-					if ipNet.IP.IsGlobalUnicast() {
-						// 添加详细的日志输出
-						log.Printf("获取到的IP地址：%s，对应网络接口：%s\n", ipNet.IP.String(), i.Name)
-						return ipNet.IP.String(), nil
-					}
-				}
+	for _, addr := range addrs {
+		ipNet, isVailIpNet := addr.(*net.IPNet)
+		if isVailIpNet && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				ip = append(ip, ipNet.IP.String())
 			}
 		}
-	}
 
-	return "", errors.New("Unable to find a valid global unicast IP address")
+	}
+	return ip
 }
 func AgentService(Address string, Port int, consulClient *api.Client) error {
 
 	Srvid := uuid.New().String()
-	ip, _ := GetIp()
+	ip := GetIp()
 	check := &api.AgentServiceCheck{
 		Interval:                       "5s",
 		Timeout:                        "5s",
-		GRPC:                           fmt.Sprintf("%v:%v", ip, Port),
+		GRPC:                           fmt.Sprintf("%v:%v", ip[0], Port),
 		DeregisterCriticalServiceAfter: "30s",
 	}
 	fmt.Println("wwwww", Port, ip)
@@ -66,7 +83,7 @@ func AgentService(Address string, Port int, consulClient *api.Client) error {
 		Name:    "user_srv",
 		Tags:    []string{"GRPC"},
 		Port:    Port,
-		Address: ip,
+		Address: ip[0],
 		Check:   check,
 	})
 	if err != nil {
